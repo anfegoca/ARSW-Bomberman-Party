0faced074f7c0b0eec56d047fc08680e3e6c4e54
Andres Gonzalez <anfegoca@gmail.com>
2020-06-30 14:51:07 -0500

    Correcciones

--- src/main/java/co/edu/escuelaing/arsw/bombermanparty/beans/App.java
+++ src/main/java/co/edu/escuelaing/arsw/bombermanparty/beans/App.java
@@ -1,5 +1,7 @@
 package co.edu.escuelaing.arsw.bombermanparty.beans;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +12,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 
+
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication app = new SpringApplication(App.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port",getPort()));
+        app.run(args);
+    }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 
 }
